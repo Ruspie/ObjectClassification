4228e1c2c9f4c9425c55b717515287f56ddb1030
Alex_Begun <begun4ik111@mail.ru>
2017-02-15 23:29:54 +0300

    Добавьте файлы проекта.

---
+++ Form.Designer.cs
@@ -0,0 +1,249 @@
+﻿namespace Test
+{
+    partial class Form
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelSettings = new System.Windows.Forms.Panel();
+            this.buttonRun = new System.Windows.Forms.Button();
+            this.labelSecondPC = new System.Windows.Forms.Label();
+            this.labelFirstPC = new System.Windows.Forms.Label();
+            this.labelFalseAlarmProbability = new System.Windows.Forms.Label();
+            this.panelResult = new System.Windows.Forms.Panel();
+            this.labelProbabilityOfTotalClassificationError = new System.Windows.Forms.Label();
+            this.textBoxProbabilityOfTotalClassificationError = new System.Windows.Forms.TextBox();
+            this.labelProbabilityMissingDetection = new System.Windows.Forms.Label();
+            this.textBoxProbabilityMissingDetection = new System.Windows.Forms.TextBox();
+            this.textBoxFalseAlarmProbability = new System.Windows.Forms.TextBox();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.numericUpDownFirstPC = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDownSecondPC = new System.Windows.Forms.NumericUpDown();
+            this.panelSettings.SuspendLayout();
+            this.panelResult.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownFirstPC)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownSecondPC)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panelSettings
+            // 
+            this.panelSettings.Controls.Add(this.numericUpDownSecondPC);
+            this.panelSettings.Controls.Add(this.numericUpDownFirstPC);
+            this.panelSettings.Controls.Add(this.buttonRun);
+            this.panelSettings.Controls.Add(this.labelSecondPC);
+            this.panelSettings.Controls.Add(this.labelFirstPC);
+            this.panelSettings.Location = new System.Drawing.Point(12, 12);
+            this.panelSettings.Name = "panelSettings";
+            this.panelSettings.Size = new System.Drawing.Size(778, 31);
+            this.panelSettings.TabIndex = 0;
+            // 
+            // buttonRun
+            // 
+            this.buttonRun.Location = new System.Drawing.Point(371, 3);
+            this.buttonRun.Name = "buttonRun";
+            this.buttonRun.Size = new System.Drawing.Size(75, 23);
+            this.buttonRun.TabIndex = 4;
+            this.buttonRun.Text = "Run";
+            this.buttonRun.UseVisualStyleBackColor = true;
+            this.buttonRun.Click += new System.EventHandler(this.buttonRun_Click);
+            // 
+            // labelSecondPC
+            // 
+            this.labelSecondPC.AutoSize = true;
+            this.labelSecondPC.Location = new System.Drawing.Point(178, 8);
+            this.labelSecondPC.Name = "labelSecondPC";
+            this.labelSecondPC.Size = new System.Drawing.Size(61, 13);
+            this.labelSecondPC.TabIndex = 2;
+            this.labelSecondPC.Text = "Second PC";
+            // 
+            // labelFirstPC
+            // 
+            this.labelFirstPC.AutoSize = true;
+            this.labelFirstPC.Location = new System.Drawing.Point(3, 8);
+            this.labelFirstPC.Name = "labelFirstPC";
+            this.labelFirstPC.Size = new System.Drawing.Size(43, 13);
+            this.labelFirstPC.TabIndex = 0;
+            this.labelFirstPC.Text = "First PC";
+            // 
+            // labelFalseAlarmProbability
+            // 
+            this.labelFalseAlarmProbability.AutoSize = true;
+            this.labelFalseAlarmProbability.Location = new System.Drawing.Point(3, 9);
+            this.labelFalseAlarmProbability.Name = "labelFalseAlarmProbability";
+            this.labelFalseAlarmProbability.Size = new System.Drawing.Size(110, 13);
+            this.labelFalseAlarmProbability.TabIndex = 2;
+            this.labelFalseAlarmProbability.Text = "False alarm probability";
+            // 
+            // panelResult
+            // 
+            this.panelResult.Controls.Add(this.labelProbabilityOfTotalClassificationError);
+            this.panelResult.Controls.Add(this.textBoxProbabilityOfTotalClassificationError);
+            this.panelResult.Controls.Add(this.labelProbabilityMissingDetection);
+            this.panelResult.Controls.Add(this.textBoxProbabilityMissingDetection);
+            this.panelResult.Controls.Add(this.labelFalseAlarmProbability);
+            this.panelResult.Controls.Add(this.textBoxFalseAlarmProbability);
+            this.panelResult.Location = new System.Drawing.Point(12, 498);
+            this.panelResult.Name = "panelResult";
+            this.panelResult.Size = new System.Drawing.Size(778, 33);
+            this.panelResult.TabIndex = 2;
+            // 
+            // labelProbabilityOfTotalClassificationError
+            // 
+            this.labelProbabilityOfTotalClassificationError.AutoSize = true;
+            this.labelProbabilityOfTotalClassificationError.Location = new System.Drawing.Point(488, 9);
+            this.labelProbabilityOfTotalClassificationError.Name = "labelProbabilityOfTotalClassificationError";
+            this.labelProbabilityOfTotalClassificationError.Size = new System.Drawing.Size(177, 13);
+            this.labelProbabilityOfTotalClassificationError.TabIndex = 6;
+            this.labelProbabilityOfTotalClassificationError.Text = "Probability of total classification error";
+            // 
+            // textBoxProbabilityOfTotalClassificationError
+            // 
+            this.textBoxProbabilityOfTotalClassificationError.Location = new System.Drawing.Point(671, 6);
+            this.textBoxProbabilityOfTotalClassificationError.Name = "textBoxProbabilityOfTotalClassificationError";
+            this.textBoxProbabilityOfTotalClassificationError.Size = new System.Drawing.Size(100, 20);
+            this.textBoxProbabilityOfTotalClassificationError.TabIndex = 5;
+            // 
+            // labelProbabilityMissingDetection
+            // 
+            this.labelProbabilityMissingDetection.AutoSize = true;
+            this.labelProbabilityMissingDetection.Location = new System.Drawing.Point(225, 9);
+            this.labelProbabilityMissingDetection.Name = "labelProbabilityMissingDetection";
+            this.labelProbabilityMissingDetection.Size = new System.Drawing.Size(151, 13);
+            this.labelProbabilityMissingDetection.TabIndex = 4;
+            this.labelProbabilityMissingDetection.Text = "Probability of missing detection";
+            // 
+            // textBoxProbabilityMissingDetection
+            // 
+            this.textBoxProbabilityMissingDetection.Location = new System.Drawing.Point(382, 6);
+            this.textBoxProbabilityMissingDetection.Name = "textBoxProbabilityMissingDetection";
+            this.textBoxProbabilityMissingDetection.Size = new System.Drawing.Size(100, 20);
+            this.textBoxProbabilityMissingDetection.TabIndex = 3;
+            // 
+            // textBoxFalseAlarmProbability
+            // 
+            this.textBoxFalseAlarmProbability.Location = new System.Drawing.Point(119, 6);
+            this.textBoxFalseAlarmProbability.Name = "textBoxFalseAlarmProbability";
+            this.textBoxFalseAlarmProbability.Size = new System.Drawing.Size(100, 20);
+            this.textBoxFalseAlarmProbability.TabIndex = 0;
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Location = new System.Drawing.Point(12, 49);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(778, 443);
+            this.pictureBox.TabIndex = 3;
+            this.pictureBox.TabStop = false;
+            // 
+            // numericUpDownFirstPC
+            // 
+            this.numericUpDownFirstPC.Cursor = System.Windows.Forms.Cursors.Default;
+            this.numericUpDownFirstPC.DecimalPlaces = 2;
+            this.numericUpDownFirstPC.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            131072});
+            this.numericUpDownFirstPC.Location = new System.Drawing.Point(52, 6);
+            this.numericUpDownFirstPC.Maximum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDownFirstPC.Name = "numericUpDownFirstPC";
+            this.numericUpDownFirstPC.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDownFirstPC.TabIndex = 5;
+            this.numericUpDownFirstPC.Value = new decimal(new int[] {
+            3,
+            0,
+            0,
+            65536});
+            this.numericUpDownFirstPC.ValueChanged += new System.EventHandler(this.numericUpDownFirstPC_ValueChanged);
+            // 
+            // numericUpDownSecondPC
+            // 
+            this.numericUpDownSecondPC.DecimalPlaces = 2;
+            this.numericUpDownSecondPC.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            131072});
+            this.numericUpDownSecondPC.Location = new System.Drawing.Point(245, 6);
+            this.numericUpDownSecondPC.Maximum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDownSecondPC.Name = "numericUpDownSecondPC";
+            this.numericUpDownSecondPC.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDownSecondPC.TabIndex = 6;
+            this.numericUpDownSecondPC.Value = new decimal(new int[] {
+            7,
+            0,
+            0,
+            65536});
+            this.numericUpDownSecondPC.ValueChanged += new System.EventHandler(this.numericUpDownSecondPC_ValueChanged);
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(797, 537);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.panelResult);
+            this.Controls.Add(this.panelSettings);
+            this.Name = "Form";
+            this.Text = "Lab3 Alex Begun 451001";
+            this.panelSettings.ResumeLayout(false);
+            this.panelSettings.PerformLayout();
+            this.panelResult.ResumeLayout(false);
+            this.panelResult.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownFirstPC)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownSecondPC)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelSettings;
+        private System.Windows.Forms.Label labelSecondPC;
+        private System.Windows.Forms.Label labelFirstPC;
+        private System.Windows.Forms.Button buttonRun;
+        private System.Windows.Forms.Label labelFalseAlarmProbability;
+        private System.Windows.Forms.Panel panelResult;
+        private System.Windows.Forms.Label labelProbabilityMissingDetection;
+        private System.Windows.Forms.TextBox textBoxProbabilityMissingDetection;
+        private System.Windows.Forms.TextBox textBoxFalseAlarmProbability;
+        private System.Windows.Forms.Label labelProbabilityOfTotalClassificationError;
+        private System.Windows.Forms.TextBox textBoxProbabilityOfTotalClassificationError;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.NumericUpDown numericUpDownSecondPC;
+        private System.Windows.Forms.NumericUpDown numericUpDownFirstPC;
+    }
+}
+
---
+++ Form.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Windows.Forms;
+using Test.Properties;
+
+namespace Test
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        private const int PointsCount = 10000;
+        private const int Offset = 100;
+        public Form()
+        {
+            InitializeComponent();
+            Bitmap bitmap = new Bitmap(pictureBox.Width, pictureBox.Height);
+            using (Graphics graphics = Graphics.FromImage(bitmap)) {
+                DrawGraph(graphics, -10);
+                pictureBox.Image = bitmap;
+            }
+           
+        }
+
+        private void buttonRun_Click(object sender, EventArgs e)
+        {
+            try {
+                double firstPc = double.Parse(numericUpDownFirstPC.Text);
+                double secondPc = double.Parse(numericUpDownSecondPC.Text);
+                ObjectClassification(firstPc, secondPc);
+            }
+            catch (Exception) {
+                MessageBox.Show(Resources.Form_buttonRun_Click_Something_went_wrong_,
+                    Resources.Form_buttonRun_Click_Error_, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+       
+
+        
+        
+
+        private void numericUpDownSecondPC_ValueChanged(object sender, EventArgs e)
+        {
+            numericUpDownFirstPC.Value = numericUpDownFirstPC.Maximum - numericUpDownSecondPC.Value;
+        }
+
+        private void numericUpDownFirstPC_ValueChanged(object sender, EventArgs e)
+        {
+            numericUpDownSecondPC.Value = numericUpDownSecondPC.Maximum - numericUpDownFirstPC.Value;
+        }
+
+        private void DrawGraph(Graphics graphics, int delimiter)
+        {
+            using (SolidBrush brush = new SolidBrush(Color.Black))
+            {
+                graphics.DrawLine(new Pen(Color.Green, 2), new Point(delimiter, 0), new Point(delimiter, pictureBox.Height));
+                graphics.DrawLine(new Pen(Color.Black, 2), new Point(0, pictureBox.Height - 1),
+                    new Point(pictureBox.Width, pictureBox.Height - 1));
+                graphics.DrawLine(new Pen(Color.Black, 2), new Point(Offset, 0),
+                    new Point(Offset, pictureBox.Height - 1));
+                graphics.DrawLine(new Pen(Color.Black, 2), new Point(Offset - 5, 15),
+                    new Point(Offset, 0));
+                graphics.DrawLine(new Pen(Color.Black, 2), new Point(Offset + 5, 15),
+                    new Point(Offset, 0));
+                graphics.DrawLine(new Pen(Color.Black, 2), new Point(pictureBox.Width - 15, pictureBox.Height - 6),
+                    new Point(pictureBox.Width, pictureBox.Height - 1));
+                graphics.DrawLine(new Pen(Color.Blue, 3), new Point(pictureBox.Width - 150, 15),
+                    new Point(pictureBox.Width - 100, 15));
+                graphics.DrawLine(new Pen(Color.Red, 3), new Point(pictureBox.Width - 150, 30),
+                    new Point(pictureBox.Width - 100, 30));
+                graphics.DrawString("p(X / C1) P(C1)", DefaultFont, brush,
+                    pictureBox.Width - 90, 5);
+                graphics.DrawString("p(X / C2) P(C2)", DefaultFont, brush,
+                    pictureBox.Width - 90, 25);
+            }
+        }
+
+        private static class ObjectClassificator
+        {
+            private static void InitializePoints(IList<int> firstPoints, IList<int> secondPoints)
+            {
+                Random random = new Random((int)DateTime.Now.Ticks);
+                for (int i = 0; i < PointsCount; i++)
+                {
+                    firstPoints[i] = random.Next(100, 550);
+                    secondPoints[i] = random.Next(0, 450);
+                }
+            }
+
+            private static void GetMu(IReadOnlyList<int> firstPoints, IReadOnlyList<int> secondPoints, out double firstMu, out double secondMu)
+            {
+                firstMu = 0;
+                secondMu = 0;
+                for (int i = 0; i < PointsCount; i++)
+                {
+                    firstMu += firstPoints[i];
+                    secondMu += secondPoints[i];
+                }
+                firstMu /= PointsCount;
+                secondMu /= PointsCount;
+            }
+
+            private static void GetSigma(int[] firstPoints, int[] secondPoints, out double firstSigma, out double secondSigma, double firstMu, double secondMu)
+            {
+                firstSigma = 0;
+                secondSigma = 0;
+                for (int i = 0; i < PointsCount; i++)
+                {
+                    firstSigma += Math.Pow(firstPoints[i] - firstMu, 2);
+                    secondSigma += Math.Pow(secondPoints[i] - secondMu, 2);
+                }
+                firstSigma = Math.Sqrt(firstSigma / PointsCount);
+                secondSigma = Math.Sqrt(secondSigma / PointsCount);
+            }
+
+            private void ObjectClassification(double firstPc, double secondPc)
+            {
+                Bitmap bitmap = new Bitmap(pictureBox.Width, pictureBox.Height);
+                Graphics graphics = Graphics.FromImage(bitmap);
+
+                int[] firstPoints = new int[PointsCount];
+                int[] secondPoints = new int[PointsCount];
+                double firstMu, secondMu;
+
+                InitializePoints(firstPoints, secondPoints);
+                GetMu(firstPoints, secondPoints, out firstMu, out secondMu);
+
+                double firstSigma, secondSigma;
+                GetSigma(firstPoints, secondPoints, out firstSigma, out secondSigma, firstMu, secondMu);
+
+                double[] firstResult = new double[pictureBox.Width];
+                double[] secondResult = new double[pictureBox.Width];
+                int delimiter = 0;
+
+                for (int x = 0; x < pictureBox.Width; x++)
+                {
+                    firstResult[x] = Math.Exp(-0.5 * Math.Pow((x - Offset - firstMu) / firstSigma, 2)) /
+                                     (firstSigma * Math.Sqrt(2 * Math.PI)) * firstPc;
+                    secondResult[x] = Math.Exp(-0.5 * Math.Pow((x - Offset - secondMu) / secondSigma, 2)) /
+                                      (secondSigma * Math.Sqrt(2 * Math.PI)) * secondPc;
+
+                    if (Math.Abs(firstResult[x] * 300 - secondResult[x] * 300) < 0.002)
+                    {
+                        delimiter = x;
+                    }
+                    if (x != 0)
+                    {
+                        graphics.DrawLine(new Pen(Color.Blue),
+                            new Point(x - 1, (pictureBox.Height - (int)(firstResult[x - 1] * pictureBox.Height * 300))),
+                            new Point(x, (pictureBox.Height - (int)(firstResult[x] * pictureBox.Height * 300))));
+                        graphics.DrawLine(new Pen(Color.Red),
+                            new Point(x - 1, (pictureBox.Height - (int)(secondResult[x - 1] * pictureBox.Height * 300))),
+                            new Point(x, (pictureBox.Height - (int)(secondResult[x] * pictureBox.Height * 300))));
+                    }
+                }
+                double falseAlarmProbability = secondResult.Take(delimiter).Sum();
+                var missingDetectionProbability = firstPc > secondPc ? secondResult.Skip(delimiter).Sum() : firstResult.Skip(delimiter).Sum();
+
+                DrawGraph(graphics, delimiter);
+
+                pictureBox.Image = bitmap;
+                textBoxFalseAlarmProbability.Text = Math.Round(falseAlarmProbability, 8).ToString(CultureInfo.CurrentCulture);
+                textBoxProbabilityMissingDetection.Text = Math.Round(missingDetectionProbability, 8).ToString(CultureInfo.CurrentCulture);
+                textBoxProbabilityOfTotalClassificationError.Text = Math.Round(falseAlarmProbability + missingDetectionProbability, 8).ToString(CultureInfo.CurrentCulture);
+                graphics.Dispose();
+            }
+        }
+    }
+}
